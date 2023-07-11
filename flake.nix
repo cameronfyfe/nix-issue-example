eec164c37d6c39ff9ecd6743a3d003697ae2d4e6
cameronfyfe <cameron.j.fyfe@gmail.com>
2023-07-11 16:58:44 -0600

    add overlay

--- flake.nix
+++ flake.nix
@@ -2,6 +2,10 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ { self, ... }:
@@ -14,12 +18,22 @@
           inherit system;
         };
 
+        pkgs2 = import inputs.nixpkgs {
+          inherit system;
+          overlays = [ inputs.rust-overlay.overlays.default ];
+        };
+
       in
       rec {
 
         devShells = {
           default = pkgs.mkShell {
             buildInputs = with pkgs; [
+              nodejs-18_x
+            ];
+          };
+          withoverlay = pkgs2.mkShell {
+            buildInputs = with pkgs2; [
               nodejs-18_x
             ];
           };
